da9c9d13d919360256f7df998c4f01c4ab23a5f0
Fred Chien <cfsghost@gmail.com>
2013-08-23 11:53:45 +0800

    add menu to app designer

--- public/components/appdesigner/menu.js
+++ public/components/appdesigner/menu.js
@@ -1,53 +1,51 @@
 enyo.kind({
 	name: 'AppDesigner.Menu',
-	kind: enyo.Component,
+	kind: 'List',
+	classes: 'enyo-unselectable',
+	reorderable: true,
 	data: [
 		{
+			icon: 'settings-4',
 			name: 'System',
 			tooltip: 'Manage system settings'
 		},
 		{
+			icon: 'database',
 			name: 'Package',
 			tooltip: 'Manage software packages'
 		},
 		{
+			icon: 'code',
 			name: 'External App',
 			tooltip: 'Manage external application'
 		}
 	],
 	components: [
 		{
-			name: 'list',
-			kind: 'List',
-			classes: 'enyo-unselectable',
-			fit: true,
-			reorderable: true,
+			name: 'item',
+			classes: 'appdesigner-menu-item',
 			components: [
-				{
-					name: 'item',
-					classes: 'appdesigner-menu-item',
-					components: [
-						{ name: 'label', classes: 'appdesigner-menu-item-label' },
-					]
-				}
-			],
-			onSetupItem: 'setupItem'
+				{ name: 'icon', kind: 'onyx.Icon' },
+				{ name: 'label', classes: 'appdesigner-menu-item-label' }
+			]
 		}
 	],
-	init: function() {
-		console.log(this);
-		//this.$.list.render();
-		//this.inherited(arguments);
-		this.$.list.setCount(this.data.length);
-		this.$.list.reset();
+	handlers: {
+		onSetupItem: 'setupItem',
+	},
+	rendered: function() {
+		this.inherited(arguments);
+		this.setCount(this.data.length);
+		this.reset();
 	},
 	setupItem: function(inSender, inEvent) {
-		console.log(123);
 		var i = inEvent.index;
 		if (!this.data[i])
 			return;
 
 		var item = this.data[i];
 		this.$.label.setContent(item.name);
+		this.$.icon.src = '/img/' + item.icon + '.png';
+		this.$.icon.srcChanged();
 	}
 });
