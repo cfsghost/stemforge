b4c89158fd9c13b0b516cc29847451405f3937aa
Fred Chien <fred@mandice.com>
2013-08-26 01:43:06 +0800

    * add example config file for testing * implement API to get package list * dynamic create components to display packages

--- engine/appdesigner.js
+++ engine/appdesigner.js
@@ -1,18 +1,21 @@
 "use strict";
 
-var apps = [
-	{ name: 'NAS', desc: 'Network attachment storage' },
-	{ name: 'Fax', desc: 'Paperless fax manchine' }
-];
+var fs = require('fs');
+var path = require('path');
 
 var AppDesigner = function() {
 	var self = this;
 };
 
-AppDesigner.prototype.getPackages = function(opts, callback) {
+AppDesigner.prototype.getPackages = function(username, projectName, opts, callback) {
 	var self = this;
 
-	callback(null, apps);
+	var projectConfigPath = path.join(__dirname, '..', 'filebase', username, projectName, 'project.json');
+	fs.readFile(projectConfigPath, 'utf8', function(err, data) {
+		var settings = JSON.parse(data);
+
+		callback(null, settings.packages);
+	});
 };
 
 module.exports = {
--- public/components/appdesigner/package/package_panel.js
+++ public/components/appdesigner/package/package_panel.js
@@ -9,16 +9,32 @@ enyo.kind({
 			classes: 'appdesigner-system-panel',
 			components: [
 				{
+					name: 'packages',
 					kind: 'onyx.Groupbox',
 					components: [
-						{ kind: 'onyx.GroupboxHeader', content: 'Packages' },
-						{ content: 'samba' },
-						{ content: 'xserver-xorg' }
+						{ kind: 'onyx.GroupboxHeader', content: 'Packages' }
 					]
 				}
 			]
 		}
 	],
 	load: function() {
+		var self = this;
+
+		// Clear package list
+		self.$.packages.destroyComponents();
+
+		var AppDesigner = App.Engine('AppDesigner');
+		AppDesigner.getPackages('fred', 'nas', {}, function(err, packages) {
+
+			for (var packageName in packages) {
+				self.$.packages.createComponent({
+					content: packageName,
+					classes: 'appdesigner-package-item'
+				});
+			}
+
+			self.$.packages.render();
+		});
 	}
 });
